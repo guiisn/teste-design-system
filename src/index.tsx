bf72d278d6a2f614c6a0a6548a0493511c629b9b
guiisn <glhrmsouzanunes@gmail.com>
2023-08-06 00:59:25 -0300

    fix: delete css

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,6 @@ import NoResults from './components/Feedbacks/NoResults';
 import DataTable from './components/Tables/DataTable';
 import Pagination from './components/Tables/Pagination';
 import GlobalTheme from './styles/theme';
-import './styles/global.css';
 
 export default function ThemeWrapper(props: { children: React.ReactNode }): JSX.Element {
   const { children } = props;
