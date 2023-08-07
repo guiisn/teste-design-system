4ab23c8851d47b1c2236decc52f8f0a1d38bd2cb
guiisn <glhrmsouzanunes@gmail.com>
2023-08-07 15:32:09 -0300

    fix: custom fonts

---
+++ src/components/CustomFonts/index.tsx
@@ -0,0 +1,51 @@
+import { Global } from '@mantine/core';
+import React from 'react';
+
+export default function CustomFonts(): JSX.Element {
+  return (
+    <Global
+      styles={[
+        {
+          '@font-face': {
+            fontFamily: 'Nunito',
+            fontStyle: 'normal',
+            fontWeight: 400,
+            src: 'url("https://fonts.gstatic.com/s/nunito/v16/XRXV3I6Li01BKofINeaB.woff2") format("woff2")',
+          },
+        },
+        {
+          '@font-face': {
+            fontFamily: 'Nunito',
+            fontStyle: 'normal',
+            fontWeight: 500,
+            src: 'url("https://fonts.gstatic.com/s/nunito/v16/XRXV3I6Li01BKofINeaB.woff2") format("woff2")',
+          },
+        },
+        {
+          '@font-face': {
+            fontFamily: 'Nunito',
+            fontStyle: 'normal',
+            fontWeight: 600,
+            src: 'url("https://fonts.gstatic.com/s/nunito/v16/XRXV3I6Li01BKofINeaB.woff2") format("woff2")',
+          },
+        },
+        {
+          '@font-face': {
+            fontFamily: 'Nunito',
+            fontStyle: 'normal',
+            fontWeight: 700,
+            src: 'url("https://fonts.gstatic.com/s/nunito/v16/XRXV3I6Li01BKofINeaB.woff2") format("woff2")',
+          },
+        },
+        {
+          '@font-face': {
+            fontFamily: 'Nunito',
+            fontStyle: 'normal',
+            fontWeight: 800,
+            src: 'url("https://fonts.gstatic.com/s/nunito/v16/XRXV3I6Li01BKofINeaB.woff2") format("woff2")',
+          },
+        },
+      ]}
+    />
+  );
+}
--- src/components/LaraProvider/index.tsx
+++ src/components/LaraProvider/index.tsx
@@ -1,12 +1,14 @@
 import { BoxProps, MantineProvider } from '@mantine/core';
 import React from 'react';
 import GlobalTheme from '../../styles/theme';
+import CustomFonts from '../CustomFonts';
 
 export default function LaraProvider(props: BoxProps): JSX.Element {
   const { children } = props;
 
   return (
     <MantineProvider theme={GlobalTheme} withGlobalStyles withNormalizeCSS>
+      <CustomFonts />
       {children}
     </MantineProvider>
   );
